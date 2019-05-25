ccdf7bcc944a0d55579f21e66b199a71d5b648d7
WangYaoSheng <wangyaosheng@zzidc.cn>
2019-05-25 18:37:51 +0800

    在admin中注册model

--- crm/admin.py
+++ crm/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 
 from crm import models
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'qq', 'source', 'consultant', 'content', 'status', 'date')
+    list_filter = ('source', 'consultant', 'date')
+    search_fields = ('qq', 'name')
+    raw_id_fields = ('consult_course',)
+    filter_horizontal = ('tags',)
+    list_editable = ('status',)
 
-admin.site.register(models.Customer)
+
+admin.site.register(models.Customer, CustomerAdmin)
 admin.site.register(models.CustomerFollowUp)
 admin.site.register(models.Enrollment)
 admin.site.register(models.Course)
@@ -15,3 +23,4 @@ admin.site.register(models.Payment)
 admin.site.register(models.StudyRecord)
 admin.site.register(models.UserProfile)
 admin.site.register(models.Tag)
+admin.site.register(models.Menu)
